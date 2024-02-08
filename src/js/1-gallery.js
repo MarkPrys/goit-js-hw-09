14c64815b7f80026752fcd21e5530fc4329e9e1b
MarkPrys <mark.prischepa@gmail.com>
2024-02-08 18:39:32 +0100

    added gallery and form

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,42 @@
-console.log("Gallery");
+import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox"; 
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+function createMarkup(array) {
+  const imagesList = array.map(({ preview, original, description }) => 
+  `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+  `).join('');
+  gallery.innerHTML = imagesList;
+  
+}
+createMarkup(galleryItems)
+
+
+  const instance = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 100,
+  });
+  
+  function handleClick(event) {
+    event.preventDefault();
+    if (event.target === event.currentTarget) {
+      return;
+    }
+    instance.open(1)
+  }
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,72 @@
-console.log("Form");
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+const btn = document.querySelector('.btn');
+const email = document.querySelector('.email');
+const message = document.querySelector('.message');
+
+email.addEventListener("input", event => {
+  localStorage.setItem("email", event.target.value);
+})
+
+message.addEventListener('input', event => {
+  localStorage.setItem("message", event.target.value)
+})
+
+
+function loadFormData() {
+  const emailData = localStorage.getItem("email");
+  const messageData = localStorage.getItem("message");
+  if (emailData) {
+    form.elements.email.value = emailData;
+  }
+  if (messageData) {
+    form.elements.message.value = messageData;
+  }
+
+}
+
+
+function clearFormData() {
+  console.log(form.elements.email.value, form.elements.message.value);
+  localStorage.removeItem("email");
+  localStorage.removeItem("message");
+  form.elements.email.value = '';
+  form.elements.message.value = '';
+
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (email.value.trim() === '' || message.value.trim() === '') {
+    alert('Поля "email" та "message" мають бути заповнені!');
+    return;
+  }
+  clearFormData()
+});
+
+window.addEventListener("load", loadFormData);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
